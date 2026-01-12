76ebc88beff431c67683b5cbad55517fa8b65648
Malania <wilsonmcw4@gmail.com>
2026-01-12 01:49:51 -0500

    Finished UI Design

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 
 namespace Notepad_V2;
 
@@ -8,4 +14,24 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+    
+    //public downloads folder
+    public static DirectoryInfo downloadsFolder = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads");
+ 
+    
+    //Delclare a delegate for the open file operation
+    
+    public delegate void OpenFileDelegate_eventhandler(IStorageFile file);
+    
+    
+    //Create an event based on the delegate
+    public event OpenFileDelegate_eventhandler OnOpenFileEvent;
+    
+    
+    
+
+    
+  
+
+    
 }
