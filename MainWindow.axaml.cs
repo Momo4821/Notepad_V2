bb5a4e4d33a8e4a52c1906a3427b7cc706f28fe5
Malania <wilsonmcw4@gmail.com>
2026-01-24 16:35:42 -0500

    1-24-2026

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
@@ -14,14 +15,66 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
+        
+        //new mainviewmodel
+        //intialize datacontext
+        
+      
+        
         InitializeComponent();
+        
     }
     
-    //public downloads folder
-    public static DirectoryInfo downloadsFolder = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads");
+    //istorage for file handling
+    
+    //top level async method for file opening
     
     
     
+    public void OpenButtonCommnad ()
+    {
+        DataContext = new Mainwindowviewmodel();
+
+        async Task OpenFileAsync()
+        {
+            var toplevel = TopLevel.GetTopLevel(this);
+            
+            
+            var files = await toplevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+                    {
+                        
+                        
+                        Title = "Open File",
+                        AllowMultiple = false,
+                      
+                        
+                    });
+                FilePickerFileType file = new FilePickerFileType ("Text Files")
+                {
+                  
+                    Patterns = new [] { "*.txt", "*.cs", "*.html", "*.css", "*.js", "*.json", "*.xml", "*.md" }
+                    
+                };
+                
+        }
+       
+        
+    }
+    
+    
+    public void NewFileButtonCommand()
+    {
+        DataContext = new Mainwindowviewmodel();
+        var topvell = DataContext as Mainwindowviewmodel;
+        
+        
+        
+        
+    }
+   
+    
+    
+
     
     
     
